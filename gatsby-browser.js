ffe4dbbc3b62f3d4a08069fa28ded9799573b1c4
Daniel Slovinsky <slovinsky1993@gmail.com>
2023-06-02 15:05:53 -0700

    Replaces npm with yarn

---
+++ gatsby-browser.js
@@ -0,0 +1,11 @@
+export const shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition,
+}) => {
+  const currentPosition = getSavedScrollPosition(location);
+  console.log(currentPosition, location.pathname);
+
+  // window.scrollTo(...(currentPosition || [0, 0]));
+
+  return true;
+};
